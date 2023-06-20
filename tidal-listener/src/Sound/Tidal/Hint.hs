62b1a42d5479732bb8313f2f39d9a12bf3f29834
polymorphicengine <gius_martin@yahoo.de>
2023-06-20 08:58:15 +0200

    try this agian

--- tidal-listener/src/Sound/Tidal/Hint.hs
+++ tidal-listener/src/Sound/Tidal/Hint.hs
@@ -20,7 +20,7 @@ import Sound.Tidal.Listener.Config
 import Sound.Tidal.Listener.Parse
 
 ghcArgs:: String -> [String]
-ghcArgs lib = ["-dynamic", "-fexternal-interpreter", "-clear-package-db", "-package-db", lib ++ "haskell-libs/package.conf.d", "-package-db", lib ++ "haskell-libs/package.db", "-L" ++ lib ++ "haskell-libs/ghc-packages","-v"]
+ghcArgs lib = ["-clear-package-db", "-package-db", lib ++ "haskell-libs/package.conf.d", "-package-db", lib ++ "haskell-libs/package.db","-v"]
 
 unsafeInterpreter :: Interpreter a -> IO (Either InterpreterError a)
 unsafeInterpreter interpreter = do
